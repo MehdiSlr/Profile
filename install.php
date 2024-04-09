c80a373cdcd5051b9ede9e6197821b64f558d1bc
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-04-09 04:22:17 +0330

    Install.php file create to install table on database

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "conf/serv_conf.php";
+
+if (isset($conn) && $conn instanceof mysqli) {
+    $sql = file_get_contents(__DIR__.'/pros.sql');
+
+    if (mysqli_multi_query($conn, $sql)) {
+        echo "Installation was successfully!";
+    }
+    else {
+        echo "Error while installation: ". mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
